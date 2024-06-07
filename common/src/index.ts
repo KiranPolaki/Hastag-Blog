a80803b73604019db1f1dbea4b86512b2f94a688
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-06-07 22:59:24 +0530

    i fucking forgot tsc -b

--- common/src/index.ts
+++ common/src/index.ts
@@ -28,9 +28,9 @@ export const updateBlogInput = z.object({
 export const forgotPassword = z.object({
   email: z.string().email(),
 });
-export type forgotPassword = z.infer<typeof forgotPassword>;
 
 export type CreateBlogInput = z.infer<typeof createBlogInput>;
 export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
 export type SignupInput = z.infer<typeof signupInput>;
 export type SigninInput = z.infer<typeof signinInput>;
+export type forgotPassword = z.infer<typeof forgotPassword>;
